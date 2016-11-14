40d1d5fad42c29db2532449d4f01947a570bafd6
Olivia D'Angelo <olivia.dangelo@gmail.com>
2016-11-13 20:53:42 -0500

    small error

--- vl_api/quickstart/views.py
+++ vl_api/quickstart/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from quickstart.serializers import UserSerializer, GroupSerializer
+from vl_api.quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
